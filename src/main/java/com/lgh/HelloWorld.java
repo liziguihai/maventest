11dea83d7b10537e65217539e0c66dbbbd7f2661
pang <pangruiqiang@ib2000.com>
2017-03-23 10:39:27 +0800

    提交一个新类 名为helloWorld 用于测试git提交

---
+++ src/main/java/com/lgh/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lgh;
+
+/**
+ * Created by ubuntu on 17-3-23.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("你好 中国！");
+    }
+}
